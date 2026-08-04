a1c601031dc1dbfef569abe4d2cc92392261db16
K-49-uX <k49ux@users.noreply.github.com>
2026-08-04 11:22:11 +0300

    complete exercise 8.5 5. Books by genre

--- library-backend/index.js
+++ library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -108,10 +108,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books
-      }
-      return books.filter((b) => b.author === args.author)
+      return books.filter((b) => {
+        const matchesAuthor = args.author ? b.author === args.author : true
+        const matchesGenre = args.genre ? b.genres.includes(args.genre) : true
+        return matchesAuthor && matchesGenre
+      })
     },
     allAuthors: () => authors,
   },
