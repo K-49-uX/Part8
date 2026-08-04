e34ca89c06fb05d15abed1099767196e3fb3bdd3
K-49-uX <k49ux@users.noreply.github.com>
2026-08-04 11:06:34 +0300

    complete exercise 8.3 3. All authors

--- library-backend/index.js
+++ library-backend/index.js
@@ -80,6 +80,13 @@ let books = [
 ]
 
 const typeDefs = `
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Book {
     title: String!
     published: Int!
@@ -92,6 +99,7 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -100,6 +108,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter((b) => b.author === root.name).length,
   },
 }
 
