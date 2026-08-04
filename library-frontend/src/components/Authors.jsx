4ad2c60619c5714c2969da2284725cb1ee02e2ff
K-49-uX <k49ux@users.noreply.github.com>
2026-08-04 15:06:26 +0300

    exercise 8.11: authors birth year

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,18 +1,35 @@
+import { useState } from 'react'
 import { gql } from '@apollo/client'
-import { useQuery } from '@apollo/client/react'
+import { useQuery, useMutation } from '@apollo/client/react'
 
-const ALL_BOOKS = gql`
+const ALL_AUTHORS = gql`
   query {
-    allBooks {
-      title
-      author
-      published
+    allAuthors {
+      name
+      born
+      bookCount
     }
   }
 `
 
-const Books = (props) => {
-  const result = useQuery(ALL_BOOKS)
+const EDIT_BIRTH_YEAR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const result = useQuery(ALL_AUTHORS)
+  const [changeBirthYear] = useMutation(EDIT_BIRTH_YEAR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
 
   if (!props.show) {
     return null
@@ -22,30 +39,63 @@ const Books = (props) => {
     return <div>loading...</div>
   }
 
-  const books = result.data ? result.data.allBooks : []
+  const authors = result.data ? result.data.allAuthors : []
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    changeBirthYear({
+      variables: {
+        name,
+        setBornTo: parseInt(born, 10),
+      },
+    })
+
+    setName('')
+    setBorn('')
+  }
 
   return (
     <div>
-      <h2>books</h2>
-
+      <h2>authors</h2>
       <table>
         <tbody>
           <tr>
             <th></th>
-            <th>author</th>
-            <th>published</th>
+            <th>born</th>
+            <th>books</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
 
-export default Books
+export default Authors
